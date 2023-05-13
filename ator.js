42cf0f10ec04788058a6a4bb5dae529a59b75cb0
shay <127038962+shayantz@users.noreply.github.com>
2023-05-13 15:18:19 -0300

    Jogo da Vaquinha - Freeway
    Jogo ciado no P5 para adquirir e reforçar conhecimento em lógica de programação e JavaScript. Link: https://editor.p5js.org/shayantz/sketches/HtXj6jI0J
    
    Para jogar, deve-se clicar no play na parte superior e depois clicar dentro da tela de jogo. As setas up e down servem para mover o personagem.

---
+++ ator.js
@@ -0,0 +1,61 @@
+
+//variaveis do ator
+
+let yAtor = 366;
+let xAtor = 86;
+let colisao = false;
+let meusPontos = 0;
+
+
+function mostraAtor() {
+   image(imagemDoAtor, xAtor, yAtor, 30, 30); 
+}
+
+function movimentaAtor() {
+  if (keyIsDown(UP_ARROW)){
+        yAtor -= 3;
+  }
+  
+  if (keyIsDown(DOWN_ARROW)){
+    if (podeSeMover){
+    yAtor += 3;
+  }}
+}
+
+function verificaColisao(){
+  for (let i = 0; i < imagensCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao) {
+      voltaAtorPosicaoInicial()
+       somColisao.play()
+      if (pontosMaiorQueZero > 0){
+        meusPontos --
+      }
+    }
+  }
+}
+function voltaAtorPosicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 240, 60))
+  text(meusPontos, width/5, 27)
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos ++
+    somPonto.play()
+    voltaAtorPosicaoInicial()
+  }
+}
+function pontosMaiorQueZero(){
+  return meusPontos > 0
+}
+
+function podeSeMover(){
+  return yAtor < 366
+}
